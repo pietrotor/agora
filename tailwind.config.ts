b40cce526f63bdfc8cc6738d7a1b0af743641034
Pietro Torrico <torricopietro@gmail.com>
2024-03-27 19:15:27 -0400

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,16 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        manrope: ["manrope", "sans-serif"],
+      },
+      colors: {
+        primary: {
+          DEFAULT: "#C1285D",
+        },
+        secondary: {
+          DEFAULT: "#19417F",
+        },
       },
     },
   },
